edafeb334254aeff3ce0511d8b4081f0ab3622c9
Guest User <Guest@epicodus-20.local>
2018-07-13 09:38:00 -0700

    Create stylist class, with properties, constructor and getters

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,8 +4,47 @@ using System;
 
 namespace HairSalon.Models
 {
-  public class Sylist
+  public class Stylist
   {
-    
+    private int _id;
+    private string _name;
+    private string _specialty;
+
+    public Stylist(string name, string specialty, int id = 0)
+    {
+      _id = id;
+      _name = name;
+      _specialty = specialty;
+    }
+
+    public override bool Equals(System.Object otherStylist)
+    {
+      if(!(otherStylist is Stylist))
+      {
+        return false;
+      }
+      else
+      {
+        Stylist newStylist = (Stylist) otherStylist;
+        return this.GetId().Equals(newStylist.GetId());
+      }
+    }
+    public override int GetHashCode()
+    {
+      return this.GetId().GetHashCode();
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public string GetSpecialty()
+    {
+      return _specialty;
+    }
   }
 }
