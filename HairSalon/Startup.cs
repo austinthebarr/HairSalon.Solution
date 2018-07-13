fdd2e1d93cf354923bbd8faf11eb7c7f6c11f0ae
Guest User <Guest@epicodus-20.local>
2018-07-13 08:38:14 -0700

    create all folders

--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ToDoList
+namespace HairSalon
 {
   public class Startup
   {
@@ -41,6 +41,6 @@ namespace ToDoList
       }
       public static class DBConfiguration
       {
-        public static string ConnectionString = "server=localhost; user id=root;password=root;port=8889;database=to_do;";
+        public static string ConnectionString = "server=localhost; user id=root;password=root;port=8889;database=first_last;";
       }
 }
