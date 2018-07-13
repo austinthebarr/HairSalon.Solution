041842b37c6e2511e1d0c92ab683540924326dff
Guest User <Guest@epicodus-20.local>
2018-07-13 13:34:07 -0700

    created Client.cs with properties and getters

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -7,6 +7,40 @@ namespace HairSalon.Models
 {
   public class Client
   {
+    private int _id;
+    private string _name;
+
+    public Client(string name, int id = 0)
+    {
+      _id = id;
+      _name = name;
+    }
+
+    public override bool Equals(System.Object otherClient)
+    {
+      if(!(otherClient is Client))
+      {
+        return false;
+      }
+      else
+      {
+        Client newClient = (Client) otherClient;
+        return this.GetId().Equals(newClient.GetId());
+      }
+    }
+    public override int GetHashCode()
+    {
+      return this.GetId().GetHashCode();
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
 
   }
 }
